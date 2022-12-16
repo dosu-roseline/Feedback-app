8dc0c89c23be2e31fa7d8d40060e9fcdcd73613c
roseline_dosu <dosuroseline@gmail.com>
2022-12-16 17:29:02 +0100

    New changes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,55 +1,24 @@
-import { motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
 
-  // return (
-  //   <div className="feedback-list">
-  //     <AnimatePresence>
-  //       {feedback.map((item) => {
-  //         <motion.div
-  //         key={item.id}
-  //         initial={{opacity: 0}}
-  //         animate={{opacity: 1}}
-  //         exit={{opacity: 0}}
-  //         >
-  //           <FeedbackItem
-  //             key={item.id}
-  //             item={item}
-  //             handleDelete={handleDelete}
-  //           />
-  //         </motion.div>
-  //       })}
-  //     </AnimatePresence>
-  //   </div>
-  // );
-
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map((item) => {
-        return (
-          <FeedbackItem key={item.id} item={item}  />
-        );
+        return <FeedbackItem key={item.id} item={item} />;
       })}
     </div>
   );
 }
-
-// FeedbackList.propsTypes = {
-//   feedback: PropsTypes.arrayOf(
-//     PropsTypes.shape({
-//       id: PropsTypes.number.isRequired,
-//       text: PropsTypes.string.isRequired,
-//       rating: PropsTypes.number.isRequired,
-//     })
-//   ),
-// };
 
 export default FeedbackList;
